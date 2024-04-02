1726602bbb90422712c25c47ad0c48ab61234a0f
Ksenofontov Gregory <49430902+gregkseno@users.noreply.github.com>
2024-04-02 21:30:27 +0300

    Fixed google chrome match, removed default layouts

--- .config/qtile/core/groups.py
+++ .config/qtile/core/groups.py
@@ -6,14 +6,14 @@ from core.keys import keys, mod
 
 groups: list[Group] = []
 
-for key, label, layout, matches in [
-    ("1", "", "max", Match(wm_class="code")),
-    ("2", "", None, Match(wm_class="alacritty")),
-    ("3", "󰈹", "max", Match(wm_class=["google-chrome", "firefox"])),
-    ("w", "󰇮", "max", Match(wm_class=["discord", "telegram-desktop"])),
-    ("e", "", "max", Match(wm_class="spotify")),
+for key, label, matches in [
+    ("1", "", Match(wm_class="code")),
+    ("2", "", Match(wm_class="Alacritty")),
+    ("3", "󰈹", Match(wm_class=["google-chrome", "firefox"])),
+    ("w", "󰇮", Match(wm_class=["discord", "telegram-desktop"])),
+    ("e", "", Match(wm_class="spotify")),
 ]:
-    groups.append(Group(key, matches, label=label, layout=layout))
+    groups.append(Group(key, matches, label=label))
 
     keys.extend([
         Key([mod], key, lazy.group[key].toscreen(toggle=True), desc=f"Switch to group {groups[-1].name}"),
